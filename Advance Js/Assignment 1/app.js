525e1e65c18ce8d442454f1de7b2004916c32a1a
Zohaib <154689818+SyedZohaib7uu@users.noreply.github.com>
2024-11-16 12:09:44 -0800

    Advance Js

---
+++ Advance Js/Assignment 1/app.js
@@ -0,0 +1,133 @@
+// q1
+// const name = "Syed Zohaib";
+// const fatherName = "Syed Shehzad";
+// const age = 14;
+// const email = "zohaib@gmail.com";
+// const phoneNumber = "00022211133";
+// const instituteName = "Jawan Pakistan";
+// const Education = "Under Matric";
+
+// document.write(`
+//     <center>
+//     <h1>BioData</h1>
+//     <br>
+//     <br>
+//     <h1>Name : ${name}</h1>
+//     <h1>Father Name : ${fatherName}</h1>
+//     <h1>Age : ${age}</h1>
+//     <h1>Email : ${email}</h1>
+//     <h1>Phone Number : ${phoneNumber}</h1>
+//     <h1>institute Name : ${instituteName}</h1>
+//     <h1>Education : ${Education}</h1>
+//     </center>
+//     `);
+
+// q1
+// q2
+// reinitialize
+// var a = 20
+// a=50
+// console.log(a);
+// var abc = "a"
+// abc=[]
+// console.log(abc);
+
+// let a = 20
+// a=50
+// console.log(a);
+// let abc = "a"
+// abc=[]
+// console.log(abc);
+
+// const a = 20
+// a=50
+// console.log(a);
+// const abc = "a"
+// abc=[]
+// console.log(abc);
+
+
+// reinitialize
+// reassign
+// var b = 20
+// b=50
+// console.log(b);
+// var cbc = "a"
+// cbc=[]
+// console.log(cbc);
+
+// let b = 20
+// b=50
+// console.log(b);
+// let cbc = "a"
+// cbc=[]
+// console.log(cbc);
+
+// const b = 20
+// b=50
+// console.log(b);
+// const cbc = "a"
+// cbc=[]
+// console.log(cbc);
+
+
+// reassign
+// hoisting
+// console.log(a);
+// var a = "abc"
+// console.log(a);
+
+
+
+// console.log(a);
+// let a = "abc"
+// console.log(a);
+
+// console.log(a);
+// const a = "abc"
+// console.log(a);
+// name()
+// function name()
+// {
+// alert("Hello")
+// }
+// function name()
+// {
+//     alert("Hello")
+// }
+// name()
+
+// greet(); 
+
+// var greet = function() {
+//     console.log("Hello!");
+// };
+
+
+// hoisting
+
+// block scope
+// function name(){
+//  var karachi = "karachi"
+    
+
+// }
+// console.log(karachi);
+// {
+//  var karachi = "karachi"
+    
+
+// }
+// console.log(karachi);
+
+// var karachi = "zohaib"
+// {
+
+//     var karachi = "shehzad"
+//     console.log(karachi);
+    
+// }
+// console.log(karachi);
+
+// block scope
+// q2
